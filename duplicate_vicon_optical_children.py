5ee6c1407704c1efe7fb167b8989c362912fafc3
AmaKenju <kenju.amauchi@gmail.com>
2026-05-05 06:59:59 +0900

    Relation

--- duplicate_vicon_optical_children.py
+++ duplicate_vicon_optical_children.py
@@ -1,8 +1,15 @@
-from pyfbsdk import FBFindModelByLabelName, FBModelNull, FBSystem
+from pyfbsdk import (
+    FBFindModelByLabelName,
+    FBModelNull,
+    FBSystem,
+    FBConstraintRelation,
+    FBConnect,
+)
 
 
 TARGET_NAME = "Vicon:Optical"
 NEW_ROOT_NAME = "Vicon:Optical_copy"
+RELATION_NAME = "Vicon_Optical_copy_Relation"
 
 
 def collect_existing_namespaces():
@@ -43,7 +50,7 @@ def split_namespace(long_name):
     return "", long_name
 
 
-def clone_hierarchy(model, new_parent, target_namespace):
+def clone_hierarchy(model, new_parent, target_namespace, pairs):
     clone = model.Clone()
     if clone is None:
         return None
@@ -52,8 +59,10 @@ def clone_hierarchy(model, new_parent, target_namespace):
     _, short = split_namespace(model.LongName)
     clone.LongName = "{0}:{1}".format(target_namespace, short)
 
+    pairs.append((model, clone))
+
     for child in list(model.Children):
-        clone_hierarchy(child, clone, target_namespace)
+        clone_hierarchy(child, clone, target_namespace, pairs)
     return clone
 
 
@@ -73,22 +82,61 @@ def duplicate_children_under_new_root(parent_name, new_root_name):
         new_root.LongName = "{0}:{1}".format(new_root_ns, new_root_short)
     new_root.Show = True
 
-    duplicates = []
+    pairs = []
     for child in list(parent.Children):
         base_ns, _ = split_namespace(child.LongName)
         if not base_ns:
             base_ns = child.Name
         new_ns = make_unique("{0}_copy".format(base_ns), existing_namespaces)
-        result = clone_hierarchy(child, new_root, new_ns)
-        if result is not None:
-            duplicates.append((new_ns, result))
+        clone_hierarchy(child, new_root, new_ns, pairs)
 
     FBSystem().Scene.Evaluate()
-    return new_root, duplicates
+    return new_root, pairs
 
 
-new_root, created = duplicate_children_under_new_root(TARGET_NAME, NEW_ROOT_NAME)
+def find_anim_node(parent_node, name):
+    if parent_node is None:
+        return None
+    for n in parent_node.Nodes:
+        if n.Name == name:
+            return n
+    return None
+
+
+def build_relation(name, pairs, channels=("Translation", "Rotation")):
+    relation = FBConstraintRelation(name)
+    relation.Active = True
+
+    y_step = 120
+    x_sender = 0
+    x_receiver = 600
+
+    connected = 0
+    for index, (source_model, dest_model) in enumerate(pairs):
+        sender_box = relation.SetAsSource(source_model)
+        receiver_box = relation.ConstrainObject(dest_model)
+
+        y = index * y_step
+        relation.SetBoxPosition(sender_box, x_sender, y)
+        relation.SetBoxPosition(receiver_box, x_receiver, y)
+
+        out_root = sender_box.AnimationNodeOutGet()
+        in_root = receiver_box.AnimationNodeInGet()
+
+        for ch in channels:
+            src = find_anim_node(out_root, ch)
+            dst = find_anim_node(in_root, ch)
+            if src is not None and dst is not None:
+                FBConnect(src, dst)
+                connected += 1
+
+    return relation, connected
+
+
+new_root, pairs = duplicate_children_under_new_root(TARGET_NAME, NEW_ROOT_NAME)
 print("Created new top-level node: '{0}'".format(new_root.LongName))
-print("Subtrees duplicated under it: {0}".format(len(created)))
-for ns, root in created:
-    print(" - namespace='{0}', root='{1}'".format(ns, root.LongName))
+print("Pairs to connect: {0}".format(len(pairs)))
+
+relation, connection_count = build_relation(RELATION_NAME, pairs)
+print("Relation '{0}' created with {1} channel connection(s).".format(
+    relation.LongName, connection_count))
